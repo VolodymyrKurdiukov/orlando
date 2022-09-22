238accdc030bb0c627b1196137d8bb1ad5c7b12b
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-09-22 11:56:32 +0300

    header

---
+++ dist/js/script.js
@@ -0,0 +1,15 @@
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
+
+$(document).ready(function () {
+	$('.header-bottom__link-arrow').click(function (event) {
+		$(this).toggleClass('active').next().toggleClass('active');
+	});
+});
