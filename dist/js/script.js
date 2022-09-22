5618581c715069298ea6f2eaf7cc1c733fb9da67
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-09-22 16:51:50 +0300

    testimonials

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,6 +10,25 @@ function ibg() {
 }
 ibg();
 
+let Slider = new Swiper('.testimonials__wrapper', {
+	wrapperClass: "testimonials__body",
+	slideClass: "testimonials__item",
+	slidesPerView: 'auto',
+	watchOverFlow: true,
+	observer: true,
+	observeParents: true,
+	observeSlideChildren: true,
+	speed: 1000,
+	centeredSlides: true,
+	spaceBetween: 120,
+	loop: true,
+	navigation: {
+		nextEl: ".testimonials__next",
+		prevEl: ".testimonials__prev",
+	}
+});
+
+
 $(document).ready(function () {
 	$('.header-bottom__link-arrow').click(function (event) {
 		$(this).toggleClass('active').next().toggleClass('active');
