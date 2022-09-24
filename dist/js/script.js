0c493820780576a8af915528198b40ecde0e684c
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-09-24 20:02:56 +0300

    faqGeneral

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,4 +33,12 @@ $(document).ready(function () {
 	$('.faq__label').click(function (event) {
 		$(this).toggleClass('active').next().slideToggle(300);
 	});
+	$('.item-faq-general__label').click(function (event) {
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
+	$(".faq-general__tab-item").not(":first").hide();
+	$(".faq-general__wrapper .faq-general__tab").click(function () {
+		$(".faq-general__wrapper .faq-general__tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".faq-general__tab-item").hide().eq($(this).index()).fadeIn();
+	}).eq(0).addClass("active");
 });
