10e27d55340e84095731325a0d2a4804a04b7414
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-10-03 09:47:46 +0300

    ed

--- dist/js/script.js
+++ dist/js/script.js
@@ -216,27 +216,25 @@ jQuery(($) => {
 });
 
 
-window.onload = function () {
-	var textPath = document.querySelector('#text-path');
+var taxtPath = document.querySelector('#text-path');
 
-	var textContainer = document.querySelector('#text-container');
+var textContainer = document.querySelector('#text-container');
 
-	var path = document.querySelector(textPath.getAttribute('href'));
+var path = document.querySelector(taxtPath.getAttribute('href'));
 
-	var pathLength = path.getTotalLength();
+var pathLength = path.getTotalLength();
 
-	function updateTextPathOffset(offset) {
-		textPath.setAttribute('startOffset', offset);
-	}
-	updateTextPathOffset(pathLength);
-
-	function onScroll() {
-		requestAnimationFrame(function () {
-			var rect = textContainer.getBoundingClientRect();
-			var scrollPercent = rect.y / window.innerHeight;
-			updateTextPathOffset(scrollPercent * pathLength);
-		});
-	}
-
-	window.addEventListener('scroll', onScroll);
+function updateTextPathOffset(offset) {
+	taxtPath.setAttribute('startOffset', offset);
 }
+updateTextPathOffset(pathLength);
+
+function onScroll() {
+	requestAnimationFrame(function () {
+		var rect = textContainer.getBoundingClientRect();
+		var scrollPercent = rect.y / window.innerHeight;
+		updateTextPathOffset(scrollPercent * pathLength);
+	});
+}
+
+window.addEventListener('scroll', onScroll);
